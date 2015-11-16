0b523b22e2290f46a1e37e1f3f2205ea6c7105bc
Mark Karavan <mark.karavan@gmail.com>
2015-11-16 18:12:32 -0500

    Adds parsing data types

--- schemeinterpreter.hs
+++ schemeinterpreter.hs
@@ -1,4 +1,52 @@
--- Scheme Interpreter v.0.0.1
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
 
-main = do
-	putStrLn "Testing the commit"
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right _ -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of 
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+main :: IO ()
+main = do 
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
+
